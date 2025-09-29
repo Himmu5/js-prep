833facb51b39842a937fc0cd41cec31e8d6e08a3
Himanshu Chauhan <adityachauhan5566@gmail.com>
2025-09-29 11:55:27 +0530

    feat: print five stars

--- pattern.js
+++ pattern.js
@@ -1,6 +1,13 @@
 // console.log("Hello")
 // console.log("World")
 
-process.stdout.write("Hello")
-process.stdout.write("World")
-console.log("hello")
+// process.stdout.write("Hello")
+// process.stdout.write("World")
+// console.log("hello")
+
+// const prompt = require("prompt-sync")()
+// const number = prompt("Enter a number: ")
+// process.stdout.write(number)
+
+// Print five star
+for(let i =0;i<5;i++) console.log("*")
