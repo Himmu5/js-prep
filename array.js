f9dcca4ac727cde9c8ea2769592bf5ca95294f6e
Himanshu Chauhan <adityachauhan5566@gmail.com>
2025-09-29 22:50:59 +0530

    feat: 0 and 1 sorting program added

--- array.js
+++ array.js
@@ -12,12 +12,40 @@
 
 
 // Max value in this array
-const arr = [4, 5, 6, 7, 12, 10, 32, 9]
-if (arr.length == 0) console.log("Array is empty!!");
-else {
-    let max = arr[0]
-    for (let i = 1; i < arr.length - 1; i++) {
-        if(arr[i] > max) max = arr[i];
+// const arr = [4, 5, 6, 7, 12, 10, 32, 9]
+// if (arr.length == 0) console.log("Array is empty!!");
+// else {
+//     let max = arr[0]
+//     for (let i = 1; i < arr.length - 1; i++) {
+//         if(arr[i] > max) max = arr[i];
+//     }
+//     console.log("max: ", max)
+// }
+
+// Second Max value in array
+// const arr = [4, 5, 6,6, 6, 7, 12, 10, 32, 32 , 32, 9]
+// let max = arr[0]
+// let sMax = max;
+
+// for (let i = 0;i<arr.length;i++){
+//     if(max < arr[i]){
+//         sMax = max;
+//         max = arr[i];
+//     }
+// }
+// console.log("max: ", max, "  sMax: ", sMax)
+
+const arr = [1, 1, 0, 1, 0, 0 , 1, 1, 1 , 0]
+let i = 1;
+let j = 0;
+
+while (i < arr.length) { 
+    if(arr[i] != arr[j] && arr[j] == 1){
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        j++
     }
-    console.log("max: ", max)
-}
+    i++
+} 
+console.log(arr)
