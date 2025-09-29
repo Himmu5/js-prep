2a7f29d85a23c4dc5328afcb5efbcd8205399618
Himanshu Chauhan <adityachauhan5566@gmail.com>
2025-09-29 21:55:28 +0530

    feat: max value in a array

--- array.js
+++ array.js
@@ -1,11 +1,23 @@
-const arr = []
+// const arr = []
 
-let prompt = require("prompt-sync")()
-let totalValues = Number(prompt("Enter length of array: "))
+// let prompt = require("prompt-sync")()
+// let totalValues = Number(prompt("Enter length of array: "))
 
-for(let i = 0;i<totalValues;i++){
-    let userInput = Number(prompt(`Enter ${i+1} value of array: `))
-    arr.push(userInput)
+// for(let i = 0;i<totalValues;i++){
+//     let userInput = Number(prompt(`Enter ${i+1} value of array: `))
+//     arr.push(userInput)
+// }
+
+// console.log(arr.join(", "))
+
+
+// Max value in this array
+const arr = [4, 5, 6, 7, 12, 10, 32, 9]
+if (arr.length == 0) console.log("Array is empty!!");
+else {
+    let max = arr[0]
+    for (let i = 1; i < arr.length - 1; i++) {
+        if(arr[i] > max) max = arr[i];
+    }
+    console.log("max: ", max)
 }
-
-console.log(arr.join(", "))
