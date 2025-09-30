3eae043aa8da1711201b772efa65a22890b47b2b
Himanshu Chauhan <adityachauhan5566@gmail.com>
2025-09-30 07:34:20 +0530

    feat: typescript file compilation done

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// const timeout = setInterval(()=>{
-//     console.log("Interval")
-// }, 2000)
-
-// setTimeout(()=>{
-//     console.log("Timeout")
-// }, 2000)
-
-// clearInterval(timeout)
-
-// const numbers = [1, 2, 3, 4, 5];
-// const total = numbers.reduce((previous, current)=>{
-//     return previous + current;
-// }, 0)
-// console.log("total: ", total)
+console.log("TS file");
+console.log("TS file");
+console.log("TS file");
+console.log("TS file");
+console.log("TS file");
+console.log("TS file");
+console.log("TS file");
+console.log("TS file");
+console.log("TS file");
+console.log("TS file");
+console.log("TS file");
