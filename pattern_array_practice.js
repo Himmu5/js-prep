4b899c7d4b59d45e0eb1bec61c42e2de7295b589
Himanshu Chauhan <adityachauhan5566@gmail.com>
2025-09-30 12:42:32 +0530

    feat: more pattern added

--- pattern_array_practice.js
+++ pattern_array_practice.js
@@ -66,16 +66,33 @@
 // }
 
 // X pattern
-const prompt  = require("prompt-sync")()
-const number  = Number(prompt("Enter a number: "))
+// const prompt  = require("prompt-sync")()
+// const number  = Number(prompt("Enter a number: "))
 
-for(let i = 0;i<number;i++){
-    for(let j = 0;j<number;j++){
-        if(i == j || i + j == number - 1){
-            process.stdout.write("*")
-        }else{
-            process.stdout.write(" ")
-        }
+// for(let i = 0;i<number;i++){
+//     for(let j = 0;j<number;j++){
+//         if(i == j || i + j == number - 1){
+//             process.stdout.write("*")
+//         }else{
+//             process.stdout.write(" ")
+//         }
+//     }
+//     process.stdout.write("\n")
+// }
+
+const prompt = require("prompt-sync")()
+const number = Number(prompt("Enter a number: "))
+
+for (let i = 0; i < number; i++) {
+    for (let j = 0; j <= i; j++) {
+        process.stdout.write("*")
+    }
+    process.stdout.write("\n")
+
+}
+for (let i = 0; i < number; i++) {
+    for (let j = number - 1; j >= i; j--) {
+        process.stdout.write("*")
     }
     process.stdout.write("\n")
 }
