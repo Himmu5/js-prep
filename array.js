ec245ceb635cb0e63b26d9ea117aa6333618b89b
Himanshu Chauhan <adityachauhan5566@gmail.com>
2025-10-02 08:13:21 +0530

    feat: more program added to arrray

--- array.js
+++ array.js
@@ -35,17 +35,46 @@
 // }
 // console.log("max: ", max, "  sMax: ", sMax)
 
-const arr = [1, 1, 0, 1, 0, 0 , 1, 1, 1 , 0]
-let i = 1;
-let j = 0;
+// const arr = [1, 1, 0, 1, 0, 0 , 1, 1, 1 , 0]
+// let i = 1;
+// let j = 0;
 
-while (i < arr.length) { 
-    if(arr[i] != arr[j] && arr[j] == 1){
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        j++
-    }
-    i++
-} 
-console.log(arr)
+// while (i < arr.length) { 
+//     if(arr[i] != arr[j] && arr[j] == 1){
+//         let temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//         j++
+//     }
+//     i++
+// } 
+// console.log(arr)
+
+// duplicate array
+
+// const arr = [1, 2, 3, 4, 5]
+// const duplicateArray = (arrInput = [], count = 2)=>{
+//     let duplicateOut = []
+//     for(let i = 0;i<count;i++){
+//         duplicateOut = [...duplicateOut, ...arrInput]
+//     }
+//     return duplicateOut
+// }
+// console.log(duplicateArray(arr, 5))
+
+// Reverse a number with inbuild methods
+// const prompt = require("prompt-sync")();
+// let userInput = prompt("Enter a number:");
+
+// console.log(userInput.split("").reverse().join(""))
+
+// Reverse a number without inbuild methods
+// const prompt = require("prompt-sync")();
+// let userInput = Number(prompt("Enter a number:"));
+// let reverseNumber = 0;
+// while(userInput > 0){
+//     let lastDigit = userInput % 10;
+//     reverseNumber = reverseNumber * 10 +lastDigit
+//     userInput =Math.floor(userInput / 10);
+// }
+// console.log("reverse number: ", reverseNumber)
