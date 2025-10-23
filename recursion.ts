07054f6aaa8f944a87228e2af29fb8f9f0fe651b
Himanshu Chauhan <adityachauhan5566@gmail.com>
2025-10-23 07:33:17 +0530

    chore: comment out factorial function and related code for clarity

--- recursion.ts
+++ recursion.ts
@@ -9,14 +9,17 @@
 // greet(5)
 
 // factorial
+// const num = 1;
 
-const num = 1;
+// const factorialNumber = ( inptNum: number ): number => {
+//     if(inptNum == 0 || inptNum == 1 ){
+//         return 1;
+//     }
+//     return inptNum * factorialNumber(inptNum - 1)
+// }
+// console.log(factorialNumber(num))
 
-const factorialNumber = ( inptNum: number ): number => {
-    if(inptNum == 0 || inptNum == 1 ){
-        return 1;
-    }
-    return inptNum * factorialNumber(inptNum - 1)
-}
 
-console.log(factorialNumber(num))
+
+// fibonachi number
+
