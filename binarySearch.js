fe6cb5cde432b58366a46984ace26059b44aebe4
Himanshu Chauhan <adityachauhan5566@gmail.com>
2025-10-27 12:21:57 +0530

    refactor: update binary search implementation for clarity and efficiency

--- binarySearch.js
+++ binarySearch.js
@@ -1,26 +1,48 @@
-const arr = [ 3, 5, 8, 10 , 16, 20 ]
-const searchItem = 10;
+// const arr = [ 3, 5, 8, 10 , 16, 20 ]
+// const searchItem = 10;
 
-const binarySearch = (searchArray, searchE)=>{
-    let start = 0;
-    let end = searchArray.length-1;
+// const binarySearch = (searchArray, searchE)=>{
+//     let start = 0;
+//     let end = searchArray.length-1;
 
-    while(start <= end){
-        let mid = Math.floor((start + end) / 2);
+//     while(start <= end){
+//         let mid = Math.floor((start + end) / 2);
         
-        if(searchArray[mid] == searchE){
-            console.log("Element is in ",mid , " index")
-            return mid;
-        }
+//         if(searchArray[mid] == searchE){
+//             console.log("Element is in ",mid , " index")
+//             return mid;
+//         }
 
-        if(searchArray[mid] > searchE){
+//         if(searchArray[mid] > searchE){
             
+//             end = mid - 1;
+//         }else if(searchArray[mid] < searchE){
+//             start = mid + 1;
+//         }
+//     }
+
+// }
+
+// binarySearch(arr, searchItem)
+
+
+
+const arr = [ 2,4, 5, 6 , 8, 9 ,11, 14, 16 , 19, 21 , 24, 26 , 28 , 31, 35 ]
+const binarySearch = (inptArr =[], target) => { 
+    let start = 0;
+    let end = arr.length -1;
+    while(start <= end){
+        let mid = Math.floor(end - ( end - start ) / 2);
+        if(inptArr[mid] == target){
+            console.log('index of target is: ', mid);
+            return;
+        }else if(inptArr[mid] > target){
             end = mid - 1;
-        }else if(searchArray[mid] < searchE){
-            start = mid + 1;
+        }else{
+            start = mid + 1
         }
     }
-
+    console.log("Target not found");
+    return;
 }
-
-binarySearch(arr, searchItem)
+binarySearch(arr, 11)
